f4c5a8c29a0cc17fb3c741823021c2aac98352b7
HACKER <jorge.novo.rodriguez.93@gmail.com>
2024-10-16 14:40:37 +0200

    refactorización skeletons

--- src/components/Experience/ExperienceSkeleton.tsx
+++ src/components/Experience/ExperienceSkeleton.tsx
@@ -1,20 +1,22 @@
 import styles from './ExperienceSkeleton.module.css';
 
 export const ExperienceSkeleton: React.FC = () => {
+    const experienceItems = [...Array(3)];
+    const experiencesLines = [...Array(7)];
+
     return (
         <ol className={styles.history}>
-            {[...Array(3)].map((_, index) => (
+            {experienceItems.map((_, index) => (
                 <li key={index} className={styles.historyItem}>
                     <div className={styles.historyItemDot}></div>
                     <div className={styles.historyItemImgContainer}></div>
                     <div className={styles.historyItemDetails}>
-                        <div className={styles.historyItemTitle}></div>
-                        <div className={styles.historyItemRole}></div>
-                        <div className={styles.historyItemDate}></div>
-                        <div className={styles.historyItemExperience}></div>
-                        <div className={styles.historyItemExperience}></div>
-                        <div className={styles.historyItemExperience}></div>
-                        <div className={styles.historyItemLink}></div>
+                        {experiencesLines.map((_, experienceIndex) => (
+                            <div
+                                key={experienceIndex}
+                                className={styles.historyItemExperience}
+                            />
+                        ))}
                     </div>
                 </li>
             ))}
